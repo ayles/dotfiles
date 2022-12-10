e7816fbee055abf7ec421956c76e4cb442798e49
ayles <selya.main@gmail.com>
2022-12-10 21:20:51 +0300

    Initial (some nvim configuration)

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,65 @@
+require "plugins"
+require "common"
+
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- set termguicolors to enable highlight groups
+vim.opt.termguicolors = true
+
+require("nvim-treesitter.configs").setup({
+  -- A list of parser names, or "all"
+  ensure_installed = { "help", "vim", "c", "cpp", "rust" },
+
+  ---- If you need to change the installation directory of the parsers (see -> Advanced Setup)
+  -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
+
+  highlight = {
+    -- `false` will disable the whole extension
+    enable = true,
+
+    -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
+    -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
+    -- Using this option may slow down your editor, and you may see some duplicate highlights.
+    -- Instead of true it can also be a list of languages
+    additional_vim_regex_highlighting = false,
+  },
+})
+
+vim.opt.clipboard = "unnamed"
+vim.opt.number = true
+vim.opt.laststatus = 3
+vim.opt.fillchars:append({
+    horiz = '━',
+    horizup = '┻',
+    horizdown = '┳',
+    vert = '┃',
+    vertleft = '┨',
+    vertright = '┣',
+    verthoriz = '╋',
+})
+
+require("nvim-tree").setup()
+vim.api.nvim_create_autocmd("BufEnter", {
+  group = vim.api.nvim_create_augroup("NvimTreeClose", {clear = true}),
+  pattern = "NvimTree_*",
+  callback = function()
+    local layout = vim.api.nvim_call_function("winlayout", {})
+    if layout[1] == "leaf" and vim.api.nvim_buf_get_option(vim.api.nvim_win_get_buf(layout[2]), "filetype") == "NvimTree" and layout[3] == nil then vim.cmd("confirm quit") end
+  end
+})
+
+local default_colors = require("kanagawa.colors").setup()
+require('kanagawa').setup({
+    globalStatus = true,
+    colors = {
+        bg = default_colors.bg_dim, 
+    },
+    overrides = {
+        WinSeparator = {
+            fg = default_colors.bg,
+        },
+    },
+})
+require('lualine').setup()
+vim.cmd.colorscheme "kanagawa"
