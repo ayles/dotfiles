3d1b2b20aca7c83ab53e5e9ef17e009f9393ffe6
Aliaksandr Seliazniou <ayles@qavm-4a22ab07.qemu>
2023-01-09 23:08:07 +0300

    Change telescope mappings

--- dot_config/nvim/after/plugin/telescope.lua
+++ dot_config/nvim/after/plugin/telescope.lua
@@ -1,4 +1,16 @@
 require('telescope').setup {
-
+    pickers = {
+        buffers = {
+            sort_mru = true,
+            mappings = {
+                i = {
+                    ['<c-d>'] = 'delete_buffer'
+                },
+                n = {
+                    ['<c-d>'] = 'delete_buffer'
+                },
+            }
+        }
+    }
 }
 
--- dot_config/nvim/lua/mappings.lua
+++ dot_config/nvim/lua/mappings.lua
@@ -3,10 +3,6 @@ local vimp = require('vimp')
 -- Telescope
 local telescope = require('telescope.builtin')
 vimp.nnoremap('<leader>f', telescope.find_files)
-vimp.nnoremap('<leader>b', function()
-    return telescope.buffers {
-        sort_mru = true,
-        ignore_current_buffer = true,
-    }
-end)
+vimp.nnoremap('<leader>g', telescope.live_grep)
+vimp.nnoremap('<leader>b', telescope.buffers)
 
